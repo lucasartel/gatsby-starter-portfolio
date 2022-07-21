612d5760208b365ae0cbef585e6880f85ce9fd33
lucasartel <lucas.arte@gmail.com>
2022-07-21 12:04:17 -0300

    Update logo.jsx

--- src/@lekoarts/gatsby-theme-jodie/icons/logo.jsx
+++ src/@lekoarts/gatsby-theme-jodie/icons/logo.jsx
@@ -1,7 +1,16 @@
 import * as React from "react"
 
 const Logo = () => (
-  <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="283.72" height="423.58" viewBox="0 0 283.72 423.58"><defs><style>.cls-1{fill:none;}.cls-2{fill:#1b34ff;}.cls-3{clip-path:url(#clip-path);}</style><clipPath id="clip-path" transform="translate(-45.89 -33.21)"><rect class="cls-1" x="-554" y="-295" width="1920" height="1080"/></clipPath></defs><title>Logo-Davi</title><rect class="cls-2" y="98.46" width="75.6" height="226.23"/><rect class="cls-2" y="347.98" width="75.6" height="75.6"/><g class="cls-3"><path class="cls-2" d="M329.61,195.56A162.2,162.2,0,0,1,167.26,357.91H146.19v-75.6s36.2-.07,48-1.65c38.65-5.16,64.81-43,64.81-83.86,0-45.38-33.88-85.38-78.08-88-10.83-.64-33.76,0-33.76,0V33.21h20.13A162.2,162.2,0,0,1,329.61,195.56" transform="translate(-45.89 -33.21)"/></g></svg>
-)
+  
+  <svg viewBox="0 0 150 150">
+    <title>Logo-Davi</title>
+	  <rect className="cls-2" height="226.23" width="75.6" y="98.46"/>
+	  <rect className="cls-2" height="75.6" width="75.6" y="347.98"/>
+	  <g className="cls-3">
+		<path className="cls-2" d="M329.61,195.56A162.2,162.2,0,0,1,167.26,357.91H146.19v-75.6s36.2-.07,48-1.65c38.65-5.16,64.81-43,64.81-83.86,0-45.38-33.88-85.38-78.08-88-10.83-.64-33.76,0-33.76,0V33.21h20.13A162.2,162.2,0,0,1,329.61,195.56" transform="translate(-45.89 -33.21)"/>
+	</g>
+</svg>
+  
+  )
 
 export default Logo
